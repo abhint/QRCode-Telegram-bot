d11e8e48c3ea13d6c68ad7cc426c064e57da3c96
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-05 21:05:10 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -4,8 +4,38 @@
 # (c) Abhijith N T ;-)
 # Thank you https://github.com/pyrogram/pyrogram :-)
 
-class Msg:
-    source = "\nsource:https://github.com/Abhijith-cloud/QRCode-Telegram-bot"
-    start = "\n<b>This is a QR code generator bot by @thankappan369</b>"
-    error = "something is went wrong\n{error} \ncontact admin @thankappan369"
-    help = "You can encode and decode QRCode\nUsage: <b>Send any URL or text, this bot can convert to QRCode.\nYou can send a QR code image and decode it.</b>"
+from info import Info
+
+class Message:
+    START_TEXT = """
+Hello {}, I am a simple Link to QR code generator with QR code decode to link bot.
+
+Made with love ❤️ by @FayasNoushad from India 🇮🇳. Contact <a href='https://telegram.me/FayasChat'>support group</a> for discussion.
+"""
+
+    HELP_USER = """
+<b><u>Help and Informations</u></b>
+
+- Send me a link I will generate the QR code of that link.
+- Send me a QR code image I will decode that image and convert to link.
+
+Made with love ❤️ by @FayasNoushad from India 🇮🇳. Contact <a href='https://telegram.me/FayasChat'>support group</a> for discussion.
+
+"""
+    ABOUT_TEXT = f"""
+<b><u>Informations About Me</u></b>
+
+- Name : <a href='https://telegram.me/{Info.BOT_USERNAME}'>{Info.BOT_NAME}</a>
+- Channel : <a href='http://telegram.me/FayasNoushad'>Fayas</a>
+- Support : <a href='http://telegram.me/FayasChat'> Fayas Chat</a>
+- Projects : <a href='http://telegram.me/FNPROJECTS'>Fayas Projects</a>
+- Language : <a href='https://www.python.org/'>Python3</a>
+- Framework : <a href='https://docs.pyrogram.org/'>Pyrogram</a>
+- Server : <a href='https://{Info.SERVER_DOMAIN}/'>{Info.SERVER}</a>
+- Credits : <a href='https://github.com/FayasNoushad/Telegraph-Uploader-Bot#credits'>Click Here</a>
+- Source : <a href='https://github.com/FayasNoushad/Telegraph-Uploader-Bot'>Click Here</a>
+
+Made with love ❤️ by @FayasNoushad from India 🇮🇳. Contact <a href='https://telegram.me/FayasChat'>support group</a> for discussion.
+"""
+    ERROR = "Something Wrong! Click help button for more... Contact <a href='https://telegram.me/FayasChat'>support group</a>."
+    
