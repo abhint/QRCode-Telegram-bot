ea3d9a9ad44d1a8c878edca35089b207a1a28d2e
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-07 08:21:50 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -9,7 +9,7 @@ async def start(client, message):
         text=Message.START_TEXT.format(message.from_user.mention),
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔰 HELP 🔰", callback_data="help"), InlineKeyboardButton("🔰 ABOUT 🔰", callback_data="about")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat')]]),
         reply_to_message_id=message.message_id
     )
 
@@ -20,17 +20,6 @@ async def help(client, message):
         text=Message.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔰 HOME 🔰", callback_data="home"), InlineKeyboardButton("🔰 ABOUT 🔰", callback_data="about")]]),
-        reply_to_message_id=message.message_id
-    )
-
-@Client.on_message(filters.command(["about"]))
-async def about(client, message):
-    await client.send_message(
-        chat_id=message.chat.id,
-        text=Message.ABOUT_TEXT,
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔰 HELP 🔰", callback_data="help"), InlineKeyboardButton("🔰 HOME 🔰", callback_data="home")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat')]]),
         reply_to_message_id=message.message_id
     )
