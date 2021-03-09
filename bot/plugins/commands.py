53790a6052cfd1b510ecc66ef2f559aec4276f4d
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-09 11:28:39 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -2,9 +2,10 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from messages import Message
 
-START_BUTTON=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
+Buttons.object():
+    START_BUTTON=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
                                    [InlineKeyboardButton('⚜ Help and Informations ⚜', callback_data='help')]])
-HELP_BUTTON=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],,
+    HELP_BUTTON=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],,
                                   [InlineKeyboardButton('⚜ Back to Home ⚜', callback_data='home')]])
 
 @Client.on_callback_query()
@@ -18,8 +19,8 @@ async def cb_handler(bot, update):
 
 @Client.on_message(filters.command(["start"]))
 async def start(bot, message):
-    await bot.send_message(chat_id=update.chat.id, text=Message.START_TEXT.format(update.from_user.mention), parse_mode="html", disable_web_page_preview=True, reply_markup=START_BUTTON, reply_to_message_id=update.message_id)
+    await bot.send_message(chat_id=update.chat.id, text=Message.START_TEXT.format(update.from_user.mention), parse_mode="html", disable_web_page_preview=True, reply_markup=Buttons.START_BUTTON, reply_to_message_id=update.message_id)
 
 @Client.on_message(filters.command(["help"]))
 async def help(bot, message):
-    await bot.send_message(chat_id=update.chat.id, text=Message.HELP_USER, parse_mode="html", disable_web_page_preview=True, reply_markup=HELP_BUTTON, reply_to_message_id=update.message_id)
+    await bot.send_message(chat_id=update.chat.id, text=Message.HELP_USER, parse_mode="html", disable_web_page_preview=True, reply_markup=Buttons.HELP_BUTTON, reply_to_message_id=update.message_id)
