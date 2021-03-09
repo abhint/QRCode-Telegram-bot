644623590c58ebfc354c96e4181cd6af5b47b286
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-09 15:31:14 +0530

    Update env.py

--- env.py
+++ env.py
@@ -4,4 +4,5 @@ import os
 class EnvData:
     BOT_TOKEN = os.environ["BOT_TOKEN"]  # from @botfather
     API_ID = int(os.environ["API_ID"])  # from https://my.telegram.org/apps
-    API_HASH = os.environ["API_HASH"]  # from https://my.telegram.org/apps
+    API_HASH = os.environ["API_HASH"]  # from https://my.telegram.org/apps 
+    UPDATE_CHANNEL = os.environ["UPDATE_CHANNEL"] # Update channel for Force Subscribe
