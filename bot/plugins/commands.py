acf1b1599bea1fa704f0113e6a00fbefbe7efa39
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-09 11:00:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -2,24 +2,37 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from messages import Message
 
+@Client.on_callback_query()
+async def cb_handler(bot, update):
+    if update.data == "home":
+        await update.message.delete()
+        await start(bot, update.message)
+    if update.data == "help":
+        await update.message.delete()
+        await help(bot, update.message)
+    if update.data == "close":
+        await update.message.delete()
+
 @Client.on_message(filters.command(["start"]))
-async def start(client, message):
-    await client.send_message(
-        chat_id=message.chat.id,
-        text=Message.START_TEXT.format(message.from_user.mention),
+async def start(bot, message):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Message.START_TEXT.format(update.from_user.mention),
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat')]]),
-        reply_to_message_id=message.message_id
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
+                                           [InlineKeyboardButton('⚜ Help and Informations ⚜', callback_data='help')]]),
+        reply_to_message_id=update.message_id
     )
 
 @Client.on_message(filters.command(["help"]))
-async def help(client, message):
-    await client.send_message(
-        chat_id=message.chat.id,
+async def help(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
         text=Message.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat')]]),
-        reply_to_message_id=message.message_id
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
+                                           [InlineKeyboardButton('⚜ Back to Home ⚜', callback_data='home')]]),
+        reply_to_message_id=update.message_id
     )
