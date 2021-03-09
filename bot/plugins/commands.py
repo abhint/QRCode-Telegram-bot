289e86f87bcb135b5a56170a38831dd00952d5d6
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-09 12:12:54 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -2,25 +2,35 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from messages import Message
 
-class.Buttons:
-    START_BUTTON=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
-                                   [InlineKeyboardButton('⚜ Help and Informations ⚜', callback_data='help')]])
-    HELP_BUTTON=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],,
-                                  [InlineKeyboardButton('⚜ Back to Home ⚜', callback_data='home')]])
-
 @Client.on_callback_query()
 async def cb_handler(bot, update):
     if update.data == "home":
-        await update.message.edit_text(chat_id=update.chat.id, text=Message.START_TEXT.format(update.from_user.mention), parse_mode="html", disable_web_page_preview=True, reply_markup=Buttons.START_BUTTON, reply_to_message_id=update.message_id)
+        await update.message.delete()
     if update.data == "help":
-        await update.message.edit_text(chat_id=update.chat.id, text=Message.HELP_USER, parse_mode="html", disable_web_page_preview=True, reply_markup=Buttons.HELP_BUTTON, reply_to_message_id=update.message_id)
+        await update.message.delete()
     if update.data == "close":
         await update.message.delete()
 
 @Client.on_message(filters.command(["start"]))
-async def start(bot, message):
-    await bot.send_message(chat_id=update.chat.id, text=Message.START_TEXT.format(update.from_user.mention), parse_mode="html", disable_web_page_preview=True, reply_markup=Buttons.START_BUTTON, reply_to_message_id=update.message_id)
+async def start(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Message.START_TEXT.format(update.from_user.mention),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
+                                           [InlineKeyboardButton('⚜ Help and Informations ⚜', callback_data='help')]]),
+        reply_to_message_id=update.message_id
+    )
 
 @Client.on_message(filters.command(["help"]))
-async def help(bot, message):
-    await bot.send_message(chat_id=update.chat.id, text=Message.HELP_USER, parse_mode="html", disable_web_page_preview=True, reply_markup=Buttons.HELP_BUTTON, reply_to_message_id=update.message_id)
+async def help(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Message.HELP_USER,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
+                                           [InlineKeyboardButton('⚜ Back to Home ⚜', callback_data='home')]]),
+        reply_to_message_id=update.message_id
+    )
