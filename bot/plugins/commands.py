83662771e82ca8264c576abdfaa6fd201c21afe9
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-16 19:36:56 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -2,17 +2,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from messages import Message
 
-@Client.on_callback_query()
-async def cb_handler(bot, update):
-    if update.data == "home":
-        await update.message.delete()
-        await start(bot, update.message)
-    if update.data == "help":
-        await update.message.delete()
-        await help(bot, update.message)
-    if update.data == "close":
-        await update.message.delete()
-
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     await bot.send_message(
@@ -22,17 +11,5 @@ async def start(bot, update):
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
                                            [InlineKeyboardButton('⚜ Help and Informations ⚜', callback_data='help')]]),
-        reply_to_message_id=update.message_id
-    )
-
-@Client.on_message(filters.command(["help"]))
-async def help(bot, update):
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Message.HELP_USER,
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
-                                           [InlineKeyboardButton('⚜ Back to Home ⚜', callback_data='home')]]),
         reply_to_message_id=update.message_id
     )
