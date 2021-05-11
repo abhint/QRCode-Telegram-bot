f6a6609c99b232dc05c579b865ca26bec060a2e9
Abhijith NT <abhijithnt64@gmail.com>
2021-05-11 13:14:03 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-# This is bot coded by Abhijith-cloud and used for educational purposes only
+# This is bot coded by Abhijith N T and used for educational purposes only
 # https://github.com/AbhijithNT
 # (c) Abhijith N T ;-)
 # Thank you https://github.com/pyrogram/pyrogram :-)
 
 class Msg:
-    source = "\nsource:https://github.com/Abhijith-cloud/QRCode-Telegram-bot"
+    source = "\nsource:https://github.com/AbhijithNT/QRCode-Telegram-bot"
     start = "\n<b>This is a QR code generator bot by @thankappan369</b>"
     error = "something is went wrong\n{error} \ncontact admin @thankappan369"
     help = "You can encode and decode QRCode\nUsage: <b>Send any URL or text, this bot can convert to QRCode.\nYou can send a QR code image and decode it.</b>"
