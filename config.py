3cf78602095b21bf8c717b5371d2fadec5244394
Abhijith N T <abhijithnt64@gmail.com>
2021-11-06 13:29:24 +0530

    Github workflow
    TypeError: int() argument must be a string, a bytes-like object or a real number, not 'NoneType'

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 # from @botfather
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 # from https://my.telegram.org/apps
-API_ID = int(os.environ.get("API_ID"))
+API_ID = os.environ.get("API_ID")
 # from https://my.telegram.org/apps
 API_HASH = os.environ.get("API_HASH")
