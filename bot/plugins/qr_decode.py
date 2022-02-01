22f6e4b0c62040fa3fc9418ee85cd0159a29e983
Abhijith N T <abhijithnt64@gmail.com>
2022-02-01 21:31:58 +0530

    username

--- bot/plugins/qr_decode.py
+++ bot/plugins/qr_decode.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
 # This is bot coded by Abhijith N T and used for educational purposes only
-# https://github.com/AbhijithNT
-# (c) Abhijith N T ;-)
-# Thank you https://github.com/pyrogram/pyrogram :-)
+# https://github.com/abhint
+# (c) Abhijith N T
+# Thank you https://github.com/pyrogram/pyrogram 
 
 from pyrogram import Client, filters
 from PIL import Image
