ee52e76b265646e0348b05fe8f8ce08e537c53d1
AbhijithNT <abhijithnt64@gmail.com>
2022-02-03 08:07:33 +0530

    made better

--- bot/plugins/qr_cmd.py
+++ bot/plugins/qr_cmd.py
@@ -1,18 +1,20 @@
 from pyrogram import Client, filters
-from messages import Msg
+from messages import (START, SOURCE,HELP)
 
-@Client.on_message(filters.command("start"))
+
+@Client.on_message(filters.command("start") & filters.private)
 async def start_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.start}{Msg.source}",
+        text=f"Hey {message.from_user.first_name},{START}{SOURCE}",
         reply_to_message_id=message.message_id,
     )
 
-@Client.on_message(filters.command(["help", "h"]))
+
+@Client.on_message(filters.command(["help", "h"]) & filters.private)
 async def help_message(client, message):
-    await client.send_message(  
+    await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.help}{Msg.source}",
+        text=f"Hey {message.from_user.first_name},{HELP}{SOURCE}",
         reply_to_message_id=message.message_id,
     )
