406a2cf11e62d93cff1c88ffe38731abe535400a
AbhijithNT <abhijithnt64@gmail.com>
2022-02-03 08:07:25 +0530

    made better

--- messages.py
+++ messages.py
@@ -3,8 +3,9 @@
 # (c) Abhijith N T 
 # Thank you https://github.com/pyrogram/pyrogram 
 
-class Msg:
-    source = "\nsource: https://github.com/abhint/QRCode-Telegram-bot/"
-    start = "\n<b>This is a QR code generator bot by @abhint</b>"
-    error = "something is went wrong\n{error} \ncontact admin @abhint"
-    help = "You can encode and decode QRCode\nUsage: <b>Send any URL or text, this bot can convert to QRCode.\nYou can send a QR code image and decode it.</b>"
+
+SOURCE = "\nsource: https://github.com/abhint/QRCode-Telegram-bot/"
+START = "\n<b>This is a QR code generator bot by @abhint</b>"
+ERROR = "something is went wrong\n{error} \ncontact admin @abhint"
+HELP = "You can encode and decode QRCode\nUsage: <b>Send any URL or text, this bot can convert to QRCode.\nYou " \
+           "can send a QR code image and decode it.</b> "
